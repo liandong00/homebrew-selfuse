d1c5a26e6bba83c1b35a2132c2d2d20bb009a5c4
liandong <liandong00@qq.com>
2025-08-05 21:29:06 +0800

    remove unnecessary comments and clean up plist options in redis@5.rb

--- Formula/redis@5.rb
+++ Formula/redis@5.rb
@@ -6,14 +6,10 @@ class RedisAT5 < Formula
   head "https://github.com/antirez/redis.git", branch: "unstable"
 
   def install
-    # Architecture isn't detected correctly on 32bit Snow Leopard without help
-    ENV["OBJARCH"] = "-arch #{MacOS.preferred_arch}"
-
     system "make", "install", "PREFIX=#{prefix}", "CC=#{ENV.cc}"
 
     %w[run db/redis log].each { |p| (var/p).mkpath }
 
-    # Fix up default conf file to match our paths
     inreplace "redis.conf" do |s|
       s.gsub! "/var/run/redis.pid", var/"run/redis.pid"
       s.gsub! "dir ./", "dir #{var}/db/redis/"
@@ -23,8 +19,6 @@ class RedisAT5 < Formula
     etc.install "redis.conf"
     etc.install "sentinel.conf" => "redis-sentinel.conf"
   end
-
-#   plist_options manual: "redis-server #{HOMEBREW_PREFIX}/etc/redis.conf"
 
   def plist
     <<~EOS
